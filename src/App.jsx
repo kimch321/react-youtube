d2bf7443650ee75c203c7e0d36e9993533c94cf0
kimch321 <cnddlf12@naver.com>
2023-10-20 13:53:10 +0900

    QueryClient 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { Outlet } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 import SearchHeader from './components/SearchHeader';
 
 
 function App() {
+  
+const queryClient = new QueryClient();
   return (
   <>
     <SearchHeader />
-    <Outlet/>
+    <QueryClientProvider client={queryClient}>
+      <Outlet/>
+    </QueryClientProvider>
   </>
   )
 }
