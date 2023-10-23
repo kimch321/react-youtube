4e67b15a817439333b3e1d095e1f2ad59ac07f33
kimch321 <cnddlf12@naver.com>
2023-10-23 15:44:31 +0900

    VideoCard가 연관 리스트로 표현되는 경우 type=list 추가하여 다른 스타일링

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,16 +2,23 @@ import React from 'react';
 import { formatAgo } from '../util/date';
 import { useNavigate } from 'react-router-dom';
 
-function VideoCard({ video }) {
+function VideoCard({ video, type }) {
   const {title, thumbnails, channelTitle, publishedAt} = video.snippet;
   const navigate = useNavigate();
+  const isList = type === 'list';
 
   return (
     // videos/watch/${video.id} 이렇게 적으면 현재 경로에 해당 주소를 추가하는 방식으로 작동한다.
     // <li onClick={() => {navigate(`videos/watch/${video.id}`, {state: {video}})}}> 
     // 최상위 경로에서 시작하는 것을 원한다면 /를 붙여주길 바란다.
-    <li onClick={() => {navigate(`/videos/watch/${video.id}`, {state: {video}})}}>
-        <img className='w-full' src={thumbnails.medium.url} alt={title} />
+    <li className={ isList ? 'flex gap-1 m-2' : ''}
+      onClick={() => {
+          navigate(`/videos/watch/${video.id}`, {state: {video}})
+        }}>
+        <img
+          className={isList ? 'w-60 mr-2' : 'w-full'}
+          src={thumbnails.medium.url}
+          alt={title} />
         <div>
             <p className='font-semibold my-2 line-clamp-2'>{title}</p>
             <p className='text-sm opacity-80'>{channelTitle}</p>
