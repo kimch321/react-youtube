cc2366d5afddec1d7251f87c01659d95ca7ea0ed
kimch321 <cnddlf12@naver.com>
2023-10-23 15:43:04 +0900

    스타일링 추가

--- src/Pages/VideoDetail.jsx
+++ src/Pages/VideoDetail.jsx
@@ -8,8 +8,8 @@ function VideoDetail() {
   const { title, channelId, channelTitle, description } = video.snippet; 
 
   return (
-    <section>
-      <article>
+    <section className='flex flex-col lg:flex-row'>
+      <article className='basis-4/6'>
         <iframe
           id="player" 
           type="text/html"
@@ -19,13 +19,13 @@ function VideoDetail() {
           frameBorder="0"
           title={title}
         />
-        <div>
-          <h2>{title}</h2> 
+        <div className='p-8'>
+          <h2 className='text-xl font-bold'>{title}</h2> 
           <ChannelInfo id={channelId} name={channelTitle} />
-          <pre>{description}</pre>
+          <pre className='whitespace-pre-wrap'>{description}</pre>
         </div>
       </article>
-      <section>
+      <section className='basis-2/6'>
         <RelatedVideos id={video.id} />
       </section>
     </section>
--- src/components/RelatedVideos.jsx
+++ src/components/RelatedVideos.jsx
@@ -12,8 +12,8 @@ function RelatedVideos({id}) {
       {isLoading && <p>Loading...</p>}
       {error && <p>Something is wrong 😅</p>}
       {videos && (
-        <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4'>
-          { videos.map(video => <VideoCard key={video.id} video={video} />)}
+        <ul>
+          { videos.map(video => <VideoCard key={video.id} video={video} type='list' />)}
         </ul>
       )}
     </>
