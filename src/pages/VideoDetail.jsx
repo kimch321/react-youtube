2dcb4dcb0fed5b8857f1be912669f05478ba6f31
cmd <107556175+kimch321@users.noreply.github.com>
2023-10-24 01:16:38 +0900

    Update VideoDetail.jsx
    오류 해결 시도 5

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -3,24 +3,25 @@ import { useLocation } from 'react-router-dom';
 import ChannelInfo from '../components/ChannelInfo';
 import RelatedVideos from '../components/RelatedVideos';
 
-function VideoDetail() {
-  const {state: {video}} = useLocation();
-  const { title, channelId, channelTitle, description } = video.snippet; 
-
+export default function VideoDetail() {
+  const {
+    state: { video },
+  } = useLocation();
+  const { title, channelId, channelTitle, description } = video.snippet;
   return (
     <section className='flex flex-col lg:flex-row'>
       <article className='basis-4/6'>
         <iframe
-          id="player" 
-          type="text/html"
-          width="100%" 
-          height="640"
-          src={`http://www.youtube.com/embed/${video.id}`}
-          frameBorder="0"
-          title="title"
+          id='player'
+          type='text/html'
+          width='100%'
+          height='640'
+          src={`https://www.youtube.com/embed/${video.id}`}
+          frameBorder='0'
+          title={title}
         />
         <div className='p-8'>
-          <h2 className='text-xl font-bold'>{title}</h2> 
+          <h2 className='text-xl font-bold'>{title}</h2>
           <ChannelInfo id={channelId} name={channelTitle} />
           <pre className='whitespace-pre-wrap'>{description}</pre>
         </div>
@@ -31,5 +32,3 @@ function VideoDetail() {
     </section>
   );
 }
-
-export default VideoDetail;
