19a10b05401fa21fde65fec92ed29054a06750a5
kimch321 <cnddlf12@naver.com>
2023-10-23 15:14:52 +0900

    연관 비디오 출력

--- src/components/RelatedVideos.jsx
+++ src/components/RelatedVideos.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
+import { useQuery } from '@tanstack/react-query';
+import VideoCard from './VideoCard';
 
 function RelatedVideos({id}) {
+  const {youtube} = useYoutubeApi();
+  const {isLoading, error, data: videos} = useQuery(['related', id], () => youtube.relatedVideos(id));
+
   return (
-    <div>
-      {id}
-    </div>
+    <>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong 😅</p>}
+      {videos && (
+        <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4'>
+          { videos.map(video => <VideoCard key={video.id} video={video} />)}
+        </ul>
+      )}
+    </>
   );
 }
 
