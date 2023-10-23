b02d891224dc1b2e5e4b3c720cbff239bfa137a4
kimch321 <cnddlf12@naver.com>
2023-10-23 15:14:35 +0900

    ChannelInfo 제작

--- src/components/ChannelInfo.jsx
+++ src/components/ChannelInfo.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
+import { useQuery } from '@tanstack/react-query';
 
 function ChannelInfo({id, name}) {
+  const {youtube} = useYoutubeApi();
+  const {data: url} = useQuery(['channel', id], () => youtube.channelImageURL(id));
+
   return (
     <div>
+      {url && <img src={url} alt={name} />}
       {name}
     </div>
   );
